d572559c28197dc2d37b509b37cc36cfb9f9d696
PavanKumar06 <spavankumard@gmail.com>
2024-11-17 19:29:43 -0500

    Added Real time S3 to Snowflake Ingestion

--- Snowflake/SpotifyPlaylist_DW_InitialSetup.sql
+++ Snowflake/SpotifyPlaylist_DW_InitialSetup.sql
@@ -20,7 +20,7 @@ CREATE OR REPLACE TABLE SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.artist (
     external_url STRING
 );
 
-CREATE TABLE SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.song (
+CREATE OR REPLACE TABLE SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.song (
     song_id STRING PRIMARY KEY,
     song_name STRING,
     duration_ms INT,
@@ -30,3 +30,86 @@ CREATE TABLE SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.song (
     album_id STRING,  -- Foreign key referencing album table
     artist_id STRING  -- Foreign key referencing artist table
 );
+
+
+
+--Create Storage Integration
+CREATE OR REPLACE STORAGE INTEGRATION Spotify_S3
+    TYPE = EXTERNAL_STAGE
+    STORAGE_PROVIDER = S3
+    ENABLED = TRUE
+    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::471112791117:role/snowflake-s3-conn'
+    STORAGE_ALLOWED_LOCATIONS = ('s3://spotify-snowflake-etl-pavan')
+    COMMENT = 'Has Access To The Spotify S3 Bucket';
+
+--Add the STORAGE_AWS_EXTERNAL_ID to the Trust Relationships of the IAM Role
+DESC INTEGRATION Spotify_S3;
+
+CREATE SCHEMA IF NOT EXISTS SpotifyPlaylist_DW.FILE_FORMATS;
+
+--Create File Format
+CREATE OR REPLACE FILE FORMAT SpotifyPlaylist_DW.FILE_FORMATS.CSV_FILEFORMAT
+    TYPE = CSV
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    TRIM_SPACE = TRUE
+    NULL_IF = ('NULL')
+    EMPTY_FIELD_AS_NULL = TRUE;
+
+CREATE SCHEMA IF NOT EXISTS SpotifyPlaylist_DW.EXTERNAL_STAGES;
+
+--Create the Stage, this allows access to the transformed_data folder 
+CREATE OR REPLACE STAGE SpotifyPlaylist_DW.EXTERNAL_STAGES.SpotifyBucket_TransformedData
+    URL = 's3://spotify-snowflake-etl-pavan/transformed_data/'
+    STORAGE_INTEGRATION = Spotify_S3
+    FILE_FORMAT = SpotifyPlaylist_DW.FILE_FORMATS.CSV_FILEFORMAT;
+
+--Test the connection, you should see all the csv files
+LIST @SpotifyPlaylist_DW.EXTERNAL_STAGES.SpotifyBucket_TransformedData;
+
+
+
+--Create Pipes to automatically load data in real-time from S3 into Snowflake
+CREATE SCHEMA IF NOT EXISTS SpotifyPlaylist_DW.PIPES;
+
+--Pipe to detect file changes from album_data and ingest into album table 
+CREATE OR REPLACE PIPE SpotifyPlaylist_DW.PIPES.ALBUM_DATA
+    AUTO_INGEST = TRUE
+    AS
+    COPY INTO SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.album
+    FROM @SpotifyPlaylist_DW.EXTERNAL_STAGES.SpotifyBucket_TransformedData/album_data;
+
+--Use the notification_channel, to add an Event to the S3 bucket. Ensure you specify the exact album_data folder.
+DESC PIPE SpotifyPlaylist_DW.PIPES.ALBUM_DATA;
+
+--Manually run the lambda function to test if data is inserted into the Table
+SELECT TOP 10 * FROM SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.album;
+
+
+--Pipe to detect file changes from artist_data and ingest into artist table 
+CREATE OR REPLACE PIPE SpotifyPlaylist_DW.PIPES.ARTIST_DATA
+    AUTO_INGEST = TRUE
+    AS
+    COPY INTO SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.artist
+    FROM @SpotifyPlaylist_DW.EXTERNAL_STAGES.SpotifyBucket_TransformedData/artist_data;
+
+--Copy the notification_channel, goto the S3 bucket -> Properties -> Add Event -> Destination Type - SQS Queue
+DESC PIPE SpotifyPlaylist_DW.PIPES.ARTIST_DATA;
+
+--Manually run the lambda function to test if data is inserted into the Table
+SELECT TOP 10 * FROM SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.artist;
+
+
+--Pipe to detect file changes from artist_data and ingest into artist table 
+CREATE OR REPLACE PIPE SpotifyPlaylist_DW.PIPES.SONG_DATA
+    AUTO_INGEST = TRUE
+    AS
+    COPY INTO SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.song
+    FROM @SpotifyPlaylist_DW.EXTERNAL_STAGES.SpotifyBucket_TransformedData/songs_data;
+
+--Specify the proper filter i.e., the exact path (PREFIX) and the file type (SUFFIX)
+DESC PIPE SpotifyPlaylist_DW.PIPES.ARTIST_DATA;
+
+--Manually run the lambda function to test if data is inserted into the Table
+SELECT TOP 10 * FROM SpotifyPlaylist_DW.SpotifyPlaylist_DW_SCHEMA.song;
